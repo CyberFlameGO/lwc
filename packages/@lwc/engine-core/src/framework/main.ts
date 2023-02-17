54b393e7f24c5662dbb507db0c3e13b2a3638637
Dale Bustad <dale@divmain.com>
2023-02-17 01:06:38 -0800

    chore: re-order the exports to nix the Rollup heisenbug

--- packages/@lwc/engine-core/src/framework/main.ts
+++ packages/@lwc/engine-core/src/framework/main.ts
@@ -5,17 +5,6 @@
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
  */
 
-// Public APIs -------------------------------------------------------------------------------------
-export { LightningElement } from './base-lightning-element';
-export { register } from './services';
-
-export { default as api } from './decorators/api';
-export { default as track } from './decorators/track';
-export { default as wire } from './decorators/wire';
-export { readonly } from './readonly';
-
-export { setFeatureFlag, setFeatureFlagForTest } from '@lwc/features';
-
 // Internal APIs used by renderers -----------------------------------------------------------------
 export { createContextProviderWithRegister } from './wiring';
 export { getComponentHtmlPrototype } from './def';
@@ -59,3 +48,14 @@ export type {
     WireContextSubscriptionPayload,
     WireContextSubscriptionCallback,
 } from './wiring';
+
+// Public APIs -------------------------------------------------------------------------------------
+export { LightningElement } from './base-lightning-element';
+export { register } from './services';
+
+export { default as api } from './decorators/api';
+export { default as track } from './decorators/track';
+export { default as wire } from './decorators/wire';
+export { readonly } from './readonly';
+
+export { setFeatureFlag, setFeatureFlagForTest } from '@lwc/features';
